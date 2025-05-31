8f169befb4824da448ed74cee4cdb541479cccf5
Max <90206985+Elsyvien@users.noreply.github.com>
2025-05-31 17:58:40 +0200

    added chatservlet

--- src/main/java/com/maxstaneker/chatapp/chatappbackend/ChatServlet.java
+++ src/main/java/com/maxstaneker/chatapp/chatappbackend/ChatServlet.java
@@ -1,4 +1,17 @@
 package com.maxstaneker.chatapp.chatappbackend;
 
+import model.Message;
+import model.User;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+
+@WebServlet(name = "chatServlet", value = "/chat")
 public class ChatServlet {
+    
 }
