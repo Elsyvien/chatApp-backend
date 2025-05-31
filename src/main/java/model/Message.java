f4fe33f8290b2decdb21e4d466998185557eff70
Max <90206985+Elsyvien@users.noreply.github.com>
2025-05-31 17:51:01 +0200

    added basic functionality to message

--- src/main/java/model/Message.java
+++ src/main/java/model/Message.java
@@ -1,4 +1,25 @@
 package model;
 
 public class Message {
+    private String sender;
+    private String content;
+    private long timestamp;
+
+    public Message(String sender, String content, long timestamp) {
+        this.sender = sender;
+        this.content = content;
+        this.timestamp = System.currentTimeMillis();
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public long getTimestamp() {
+        return timestamp;
+    }
 }
